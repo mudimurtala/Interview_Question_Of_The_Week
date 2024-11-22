c8d95932da82272146e9957b667055c29cb48764
mudimurtala <ibnmuhyideen95@gmail.com>
2024-11-22 03:44:50 +0100

    Create variables for price and profit and loop ovr my stock list

--- issue_#379/script.js
+++ issue_#379/script.js
@@ -1,1 +1,15 @@
 const stock_prices = [10, 16, 5, 25, 11, 9, 2, 18, 20, 12, 25, 7, 14, 8];
+
+
+let lowest_price = stock_prices[0];
+let highest_profit = 0;
+
+stock_prices.forEach((price) => {
+    const profit = price - lowest_price;
+    if (profit > highest_profit) {
+        highest_profit = profit;
+    }
+    if (price < lowest_price) {
+        lowest_price = price;
+    }
+});
